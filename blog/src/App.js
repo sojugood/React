6bf6cf69154daa948374f7ed0502096897b0bba9
sojugood <tkddlrtla@naver.com>
2023-12-31 06:14:53 +0900

    done 5

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
-  let post = '강남 우동 맛집';
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{ post }</h4>
+
+      <button onClick={ () => {
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>가나다순정렬</button>
+
+      <button onClick={ () => {
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      }}>글 수정</button>
+
+      <div className="list">
+        <h4>{ 글제목[0] } <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span> { 따봉 } </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
